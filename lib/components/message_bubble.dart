5f3f4dbfbc339a780e2dcf874f540630a3ee1dc6
Tiago Web <tiagoweb@hotmail.com>
2022-06-07 11:42:43 -0400

    add componente message

---
+++ lib/components/message_bubble.dart
@@ -0,0 +1,98 @@
+import 'dart:io';
+
+import 'package:chat/core/models/chat_message.dart';
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  static const _defaultImage = 'assets/images/avatar.png';
+  final ChatMessage message;
+  final bool belongsToCurrentUser;
+
+  const MessageBubble({
+    Key? key,
+    required this.message,
+    required this.belongsToCurrentUser,
+  }) : super(key: key);
+
+  Widget _showUserImage(String imageUrl) {
+    ImageProvider? provider;
+    final uri = Uri.parse(imageUrl);
+
+    if (uri.scheme.contains('http')) {
+      provider = NetworkImage(uri.toString());
+    } else if (uri.path.contains(_defaultImage)) {
+      provider = const AssetImage(_defaultImage);
+    } else {
+      provider = FileImage(File(uri.toString()));
+    }
+
+    return CircleAvatar(
+      // backgroundColor: Colors.pink,
+      backgroundImage: provider,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Row(
+          mainAxisAlignment: belongsToCurrentUser
+              ? MainAxisAlignment.end
+              : MainAxisAlignment.start,
+          children: [
+            Container(
+              width: 180,
+              padding: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
+              ),
+              margin: const EdgeInsets.symmetric(
+                vertical: 15,
+                horizontal: 8,
+              ),
+              decoration: BoxDecoration(
+                color: belongsToCurrentUser
+                    ? Colors.grey.shade300
+                    : Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(12),
+                  topRight: const Radius.circular(12),
+                  bottomLeft: belongsToCurrentUser
+                      ? const Radius.circular(12)
+                      : const Radius.circular(0),
+                  bottomRight: belongsToCurrentUser
+                      ? const Radius.circular(0)
+                      : const Radius.circular(12),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    message.userName,
+                    style: TextStyle(
+                      color: belongsToCurrentUser ? Colors.black : Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    message.text,
+                    style: TextStyle(
+                      color: belongsToCurrentUser ? Colors.black : Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Positioned(
+          top: 0,
+          left: belongsToCurrentUser ? null : 165,
+          right: belongsToCurrentUser ? 165 : null,
+          child: _showUserImage(message.userImageUrl),
+        ),
+      ],
+    );
+  }
+}
