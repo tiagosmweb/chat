89551e4e21de3baf11b95fe8ec953167d6747974
Tiago Web <tiagoweb@hotmail.com>
2022-06-07 10:48:30 -0400

    pequeno ajustes name chat service

--- lib/core/services/chat/chat_mock_service.dart
+++ lib/core/services/chat/chat_mock_service.dart
@@ -4,7 +4,7 @@ import 'package:chat/core/models/chat_message.dart';
 import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/chat/chat_service.dart';
 
-abstract class ChatMockService implements ChatService {
+class ChatMockService implements ChatService {
   static final List<ChatMessage> _msgs = [
     ChatMessage(
       id: '1',
