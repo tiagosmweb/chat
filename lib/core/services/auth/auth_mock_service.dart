53fe326bbe5cc97536f193b54671f5d714ada0e6
Tiago Web <tiagoweb@hotmail.com>
2022-06-07 11:43:02 -0400

    ajuste dado estatico

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -7,9 +7,9 @@ import 'package:chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
   static const _defaultUser = ChatUser(
-    id: '1',
-    name: 'Teste',
-    email: 'teste@gmail.com',
+    id: '456',
+    name: 'Marina',
+    email: 'marina@gmail.com',
     imageUrl: 'assets/images/avatar.png',
   );
 
