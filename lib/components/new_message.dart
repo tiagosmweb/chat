bf412115c76a155cf81c8b8ce16207b9dcd266c4
Tiago Web <tiagoweb@hotmail.com>
2022-06-09 20:49:26 -0400

    update new message

--- lib/components/new_message.dart
+++ lib/components/new_message.dart
@@ -1,12 +1,48 @@
+import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:chat/core/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
-class NewMessage extends StatelessWidget {
+class NewMessage extends StatefulWidget {
   const NewMessage({Key? key}) : super(key: key);
 
   @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  late String _message = '';
+  final _msgController = TextEditingController();
+
+  Future<void> _sendMessage() async {
+    final user = AuthService().currentUser;
+
+    if (user != null) {
+      await ChatService().save(_message, user);
+      _msgController.clear();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('NewMessage'),
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _msgController,
+            onChanged: (msg) => setState(() => _message = msg),
+            decoration: const InputDecoration(labelText: 'Enviar menssagem...'),
+            onSubmitted: (_) {
+              if (_message.trim().isNotEmpty) {
+                _sendMessage();
+              }
+            },
+          ),
+        ),
+        IconButton(
+          onPressed: _message.trim().isEmpty ? null : _sendMessage,
+          icon: const Icon(Icons.send),
+        )
+      ],
     );
   }
 }
