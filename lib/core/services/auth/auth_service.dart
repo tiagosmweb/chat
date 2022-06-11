f2b4e052aa9462afa112810dd7e8779c742db1fa
Tiago Web <tiagoweb@hotmail.com>
2022-06-11 12:09:19 -0400

    auth firebase comment auth mock

--- lib/core/services/auth/auth_service.dart
+++ lib/core/services/auth/auth_service.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 
 import 'package:chat/core/models/chat_user.dart';
-import 'package:chat/core/services/auth/auth_mock_service.dart';
+import 'package:chat/core/services/auth/auth_firebase_service.dart';
+// import 'package:chat/core/services/auth/auth_mock_service.dart';
 
 abstract class AuthService {
   ChatUser? get currentUser;
@@ -23,6 +24,7 @@ abstract class AuthService {
   Future<void> logout();
 
   factory AuthService() {
-    return AuthMockService();
+    // return AuthMockService();
+    return AuthFirebaseService();
   }
 }
