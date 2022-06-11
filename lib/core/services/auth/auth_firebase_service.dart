fdb8fe11e99024950f1155618267d3a9aa08ce1c
Tiago Web <tiagoweb@hotmail.com>
2022-06-11 18:18:45 -0400

    ajuste load  name e avatar usuarios apos criacao e login

--- lib/core/services/auth/auth_firebase_service.dart
+++ lib/core/services/auth/auth_firebase_service.dart
@@ -52,7 +52,8 @@ class AuthFirebaseService implements AuthService {
     await credential.user?.updatePhotoURL(imageUrl);
 
     //3. salvar o usuário no banco de dados (opcioonal)
-    await _saveChatUser(_toChatUser(credential.user!, imageUrl));
+    _currentUser = _toChatUser(credential.user!, name, imageUrl);
+    await _saveChatUser(_currentUser!);
   }
 
   @override
@@ -92,10 +93,10 @@ class AuthFirebaseService implements AuthService {
     });
   }
 
-  static ChatUser _toChatUser(User user, [String? imageUrl]) {
+  static ChatUser _toChatUser(User user, [String? name, String? imageUrl]) {
     return ChatUser(
       id: user.uid,
-      name: user.displayName ?? user.email!.split('@')[0],
+      name: name ?? user.displayName ?? user.email!.split('@')[0],
       email: user.email!,
       imageUrl: imageUrl ?? user.photoURL ?? 'assets/images/avatar.png',
     );
