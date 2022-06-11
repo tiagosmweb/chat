eed3fad2f7884e4f5e5b99be271c2fd6aca3e034
Tiago Web <tiagoweb@hotmail.com>
2022-06-11 16:53:01 -0400

    simple conversion chat message

--- lib/core/services/chat/chat_firebase_service.dart
+++ lib/core/services/chat/chat_firebase_service.dart
@@ -7,15 +7,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class ChatFirebaseService implements ChatService {
   @override
   Stream<List<ChatMessage>> messagesStream() {
-    return Stream<List<ChatMessage>>.empty();
+    return const Stream<List<ChatMessage>>.empty();
   }
 
   @override
   Future<ChatMessage?> save(String text, ChatUser user) async {
     final storage = FirebaseFirestore.instance;
 
-    //ChatMessage => Map<String, dynamic>
-    storage.collection('chat').add({
+    // ChatMessage => Map<String, dynamic>
+    final docRef = await storage.collection('chat').add({
       'text': text,
       'createdAt': DateTime.now().toIso8601String(),
       'userId': user.id,
@@ -23,6 +23,17 @@ class ChatFirebaseService implements ChatService {
       'userImageUrl': user.imageUrl,
     });
 
-    return null;
+    final doc = await docRef.get();
+    final data = doc.data()!;
+
+    // Map<String, dynamic> => ChatMessage
+    return ChatMessage(
+      id: doc.id,
+      text: data['text'],
+      createdAt: DateTime.parse(data['createdAt']),
+      userId: data['userId'],
+      userName: data['userName'],
+      userImageUrl: data['userImageUrl'],
+    );
   }
 }
